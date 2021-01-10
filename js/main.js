d729fde329b87d89cdb107116b7a52e6e42e722e
DionWeb <dionysiostsentidis@gmail.com>
2021-01-10 03:05:52 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("openbutton").style.display = "none";
-    document.getElementById("closebutton").style.display = "block";
+   document.getElementById('mySidenav').style.width = '250px';
+   document.getElementById('openbutton').style.display = 'none';
+   document.getElementById('closebutton').style.display = 'block';
 }
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("openbutton").style.display = "block";
-    document.getElementById("closebutton").style.display = "none";
+   document.getElementById('mySidenav').style.width = '0';
+   document.getElementById('openbutton').style.display = 'block';
+   document.getElementById('closebutton').style.display = 'none';
 }
 
-
-$(document).on('click', 'a[href^="#"]', function(e) {
-    var id = $(this).attr('href');
-    var $id = $(id);
-    if ($id.length === 0) {
-        return;
-    }
-    e.preventDefault();
-    var pos = $id.offset().top;
-    $('body, html').animate({scrollTop: pos});
+$(document).on('click', 'a[href^="#"]', function (e) {
+   var id = $(this).attr('href');
+   var $id = $(id);
+   if ($id.length === 0) {
+      return;
+   }
+   e.preventDefault();
+   var pos = $id.offset().top;
+   $('body, html').animate({ scrollTop: pos });
 });
