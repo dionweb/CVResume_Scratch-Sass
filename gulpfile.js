a2dade825a8c2829fca1a60745bb64561c224bf5
DionWeb <dionysiostsentidis@gmail.com>
2021-01-10 02:46:37 +0100

    fix gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,18 @@
-"use strict";
+'use strict';
 
-const browserSync = require("browser-sync").create();
-const gulp = require("gulp");
-const sass = require("gulp-sass");
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 
-sass.compiler = require("node-sass");
+sass.compiler = require('node-sass');
 
-gulp.task("sass", function () {
-    return gulp
-        .src("src/scss/*.scss")
-        .pipe(sass().on("error", sass.logError))
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
+gulp.task('sass', function () {
+   return gulp
+      .src('src/scss/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('src/css'));
 });
 
-gulp.task("default", () => {
-    browserSync.init({
-        server: "./src",
-    });
-    gulp.watch("src/scss/*.scss", gulp.series("sass"));
-    gulp.watch("src/*.html").on("change", browserSync.reload);
+gulp.task('default', () => {
+   gulp.watch('src/scss/*.scss', gulp.series('sass'));
+   gulp.watch('src/*.html');
 });
